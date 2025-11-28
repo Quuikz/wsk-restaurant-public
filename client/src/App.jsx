87aa703b71853887b0dcdc10d9578b7dc509c8ec
Kasanen <veijok119@gamil.com>
2025-11-28 17:52:13 +0200

    NavBar Issues: Burger menu has unnecessary padding that is implemented from navbar. - Veijo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-
-
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import {BrowserRouter, Routes, Route} from 'react-router';
 import Layout from './components/Layout.jsx';
 import Home from './views/Home';
 import Profile from './views/Profile';
@@ -15,23 +13,38 @@ import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 const App = () => {
   return (
-     <BrowserRouter basename={import.meta.env.BASE_URL}>
-       <UserProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <UserProvider>
         <Routes>
-         <Route element={<Layout />}>
-           <Route path="/" element={<Home />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
 
-           <Route path="/profile" element={<ProtectedRoute> <Profile /></ProtectedRoute>}/>
-           <Route path="/upload" element={<ProtectedRoute> <Upload /> </ProtectedRoute>} />
-           <Route path="/single" element={<Single />} />
-           <Route path="/login" element={<Login />}/>
-           <Route path="/logout" element={<Logout />} /> 
-         </Route>
-       </Routes>
-       </UserProvider>
-     </BrowserRouter>
-   );
-    
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  {' '}
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/upload"
+              element={
+                <ProtectedRoute>
+                  {' '}
+                  <Upload />{' '}
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/single" element={<Single />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
