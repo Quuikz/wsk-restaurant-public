1cab8610c935e78cb27226b0ed162d2036fe2f40
Kasanen <veijok119@gamil.com>
2025-12-07 17:34:13 +0200

    Weeklist Done - Only couple improvements in need
    -Veijo

--- client/src/views/customer/Weeklist.jsx
+++ client/src/views/customer/Weeklist.jsx
@@ -1,15 +1,20 @@
 import {useEffect, useMemo, useState} from 'react';
-import { useMenuCommon } from '../../hooks/common/apiHooks.js';
+import {useMenuCommon} from '../../hooks/common/apiHooks.js';
 import WeeklistBox from './Weeklist/WeeklistBox';
 
 const Weeklist = () => {
   const {getAllMenuItems} = useMenuCommon();
   const [menuItems, setMenuItems] = useState([]);
   // Set current week number here
-  // Currently hardcoded to 12 and 49 for testing
-  const [currentWeek, setCurrentWeek] = useState(49);
+  // Currently hardcoded to 12 for testing
+  const initialWeek = 12;
+  const [currentWeek, setCurrentWeek] = useState(initialWeek);
 
-  // !!! Add buttons to change week number later
+  // Button managers for week navigation
+  const min = initialWeek;
+  const max = initialWeek + 3;
+  const inc = (set) => () => set((v) => Math.min(max, v + 1));
+  const dec = (set) => () => set((v) => Math.max(min, v - 1));
 
   useEffect(() => {
     // Load all menu items
@@ -34,6 +39,7 @@ const Weeklist = () => {
     <>
       <div className="max-w-7xl mx-auto">
         <div className="p-7 pt-20 pb-30 bg-orange-100">
+          {console.log(max)}
           {/* Page title */}
           <div className="text-center w-full pb-10 ">
             <h2 className="text-3xl font-medium">| Viikko {currentWeek} |</h2>
@@ -42,17 +48,22 @@ const Weeklist = () => {
 
           {/* Weekly list */}
           <div className="grid grid-cols-3 gap-4 ">
-            {console.log(menuItems)}
             {filteredMenuItems.map((menuItem) => (
               <WeeklistBox key={menuItem.id} menuItem={menuItem} />
             ))}
           </div>
 
           <div className="grid grid-cols-2 mt-10">
-            <button className="mt-4 mb-4 bg-orange-200  px-4 py-2 rounded hover:bg-orange-300 width-fit mr-auto">
+            <button
+              className="mt-4 mb-4 bg-orange-200  px-4 py-2 rounded hover:bg-orange-300 width-fit mr-auto"
+              onClick={dec(setCurrentWeek)}
+            >
               ← Viime viikko
             </button>
-            <button className="mt-4 mb-4 bg-orange-200  px-4 py-2 rounded hover:bg-orange-300 width-fit ml-auto">
+            <button
+              className="mt-4 mb-4 bg-orange-200  px-4 py-2 rounded hover:bg-orange-300 width-fit ml-auto"
+              onClick={inc(setCurrentWeek)}
+            >
               Ensi viikko →
             </button>
           </div>
