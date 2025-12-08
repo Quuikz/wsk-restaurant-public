3aa2ae549f8cef8412dff6fdb7b319e0a9b061e0
Quuikz <rkuikka@gmail.com>
2025-12-08 14:51:21 +0200

    working hsl-api

--- runTestHslApi.js
+++ runTestHslApi.js
@@ -7,67 +7,112 @@ import { getHslData } from "./server/src/api/models/hsl-model.js";
 
 async function test() {
   const query = `
-  query {
-    stop(id: "HSL:H0438") {
-      name
-      lat
-      lon
-      stoptimesWithoutPatterns(numberOfDepartures: 5) {
-        realtime
-        realtimeState
-        scheduledDeparture
-        realtimeDeparture
-        departureDelay
-        headsign
-        trip {
-          route {
-            shortName
-            mode
-          }
+query {
+  stop1: stop(id: "HSL:1070419") {
+    gtfsId
+    name
+    lat
+    lon
+    stoptimesWithoutPatterns(numberOfDepartures: 5) {
+      realtime
+      realtimeState
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      headsign
+      trip {
+        route {
+          shortName
+          mode
         }
       }
     }
+  }
 
-    stop2: stop(id: "HSL:H0439") {
-      name
-      lat
-      lon
-      stoptimesWithoutPatterns(numberOfDepartures: 5) {
-        realtime
-        realtimeState
-        scheduledDeparture
-        realtimeDeparture
-        departureDelay
-        headsign
-        trip {
-          route {
-            shortName
-            mode
-          }
+  stop2: stop(id: "HSL:1070420") {
+    gtfsId
+    name
+    lat
+    lon
+    stoptimesWithoutPatterns(numberOfDepartures: 5) {
+      realtime
+      realtimeState
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      headsign
+      trip {
+        route {
+          shortName
+          mode
         }
       }
     }
+  }
 
-    stop3: stop(id: "HSL:H0708") {
-      name
-      lat
-      lon
-      stoptimesWithoutPatterns(numberOfDepartures: 5) {
-        realtime
-        realtimeState
-        scheduledDeparture
-        realtimeDeparture
-        departureDelay
-        headsign
-        trip {
-          route {
-            shortName
-            mode
-          }
+  stop3: stop(id: "HSL:1070425") {
+    gtfsId
+    name
+    lat
+    lon
+    stoptimesWithoutPatterns(numberOfDepartures: 5) {
+      realtime
+      realtimeState
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      headsign
+      trip {
+        route {
+          shortName
+          mode
         }
       }
     }
-  }  
+  }
+
+  stop4: stop(id: "HSL:1070421") {
+    gtfsId
+    name
+    lat
+    lon
+    stoptimesWithoutPatterns(numberOfDepartures: 5) {
+      realtime
+      realtimeState
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      headsign
+      trip {
+        route {
+          shortName
+          mode
+        }
+      }
+    }
+  }
+
+  stop5: stop(id: "HSL:1070422") {
+    gtfsId
+    name
+    lat
+    lon
+    stoptimesWithoutPatterns(numberOfDepartures: 5) {
+      realtime
+      realtimeState
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      headsign
+      trip {
+        route {
+          shortName
+          mode
+        }
+      }
+    }
+  }
+}
 `;
 
   try {
@@ -82,44 +127,71 @@ test();
 
 /*
 {
-  "data": {
-      "stopsByRadius": {
-          "edges": [
-              {
-                  "node": {
-                      "stop": {
-                          "name": "Neitsytpolku",
-                          "lat": 60.158524,
-                          "lon": 24.949516,
-                          "code": "H0438"
-                      },
-                      "distance": 158
-                  }
-              },
-              {
-                  "node": {
-                      "stop": {
-                          "name": "Neitsytpolku",
-                          "lat": 60.158488,
-                          "lon": 24.949809,
-                          "code": "H0439"
-                      },
-                      "distance": 193
-                  }
-              },
-              {
-                  "node": {
-                      "stop": {
-                          "name": "Tarkk´ampujankatu",
-                          "lat": 60.161023,
-                          "lon": 24.947501,
-                          "code": "H0708"
-                      },
-                      "distance": 232
-                  }
-              }
-          ]
-      }
-  }
+    "data": {
+        "stopsByRadius": {
+            "edges": [
+                {
+                    "node": {
+                        "stop": {
+                            "name": "Neitsytpolku",
+                            "lat": 60.158524,
+                            "lon": 24.949516,
+                            "code": "H0438",
+                            "gtfsId": "HSL:1070102"
+                        },
+                        "distance": 158
+                    }
+                },
+                {
+                    "node": {
+                        "stop": {
+                            "name": "Neitsytpolku",
+                            "lat": 60.158524,
+                            "lon": 24.949516,
+                            "code": "H0438",
+                            "gtfsId": "HSL:1070419"
+                        },
+                        "distance": 158
+                    }
+                },
+                {
+                    "node": {
+                        "stop": {
+                            "name": "Neitsytpolku",
+                            "lat": 60.158488,
+                            "lon": 24.949809,
+                            "code": "H0439",
+                            "gtfsId": "HSL:1070420"
+                        },
+                        "distance": 193
+                    }
+                },
+                {
+                    "node": {
+                        "stop": {
+                            "name": "Neitsytpolku",
+                            "lat": 60.158483,
+                            "lon": 24.949833,
+                            "code": "H0439",
+                            "gtfsId": "HSL:1070103"
+                        },
+                        "distance": 193
+                    }
+                },
+                {
+                    "node": {
+                        "stop": {
+                            "name": "Tarkk´ampujankatu",
+                            "lat": 60.161023,
+                            "lon": 24.947501,
+                            "code": "H0708",
+                            "gtfsId": "HSL:1070425"
+                        },
+                        "distance": 232
+                    }
+                }
+            ]
+        }
+    }
 }
 */
