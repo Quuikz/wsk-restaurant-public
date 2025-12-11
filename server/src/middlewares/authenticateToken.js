8cb5faa7c3d27a4e45ac67769d60ec4da3cd0fc9
Quuikz <rkuikka@gmail.com>
2025-12-11 15:32:37 +0200

    Changed log to log only authorization

--- server/src/middlewares/authenticateToken.js
+++ server/src/middlewares/authenticateToken.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
 //imports
 import jwt from "jsonwebtoken";
-
 
 /**
  * Middleware, authenticates token in request header:  request.headers['authorization']
@@ -14,26 +13,25 @@ import jwt from "jsonwebtoken";
  * @returns {*}
  */
 const authenticateToken = (req, res, next) => {
-    console.log('authenticateToken', req.headers);
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    console.log('token', token);
+  console.log("authenticateToken", req.headers.authorization);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("token", token);
 
-    if (token == null) {
-        console.log('token is null in authenticateToken');
-        return res.sendStatus(401);
-    }
+  if (token == null) {
+    console.log("token is null in authenticateToken");
+    return res.sendStatus(401);
+  }
 
-    try {
-        res.locals.user = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('token ok in authenticateToken');
-        console.log(res.locals.user);
-        next();
-
-    } catch (err) {
-        console.log('invalid token');
-        res.status(403).send({message: 'invalid token'});
-    }
+  try {
+    res.locals.user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("token ok in authenticateToken");
+    console.log(res.locals.user);
+    next();
+  } catch (err) {
+    console.log("invalid token");
+    res.status(403).send({ message: "invalid token" });
+  }
 };
 
 export default authenticateToken;
