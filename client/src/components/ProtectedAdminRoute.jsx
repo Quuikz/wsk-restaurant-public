fabdb9907e66fe4c20519530e8e8dcdb7bd65a33
Arazm1 <araz.muhamm3d@gmail.com>
2025-12-11 22:33:39 +0200

    JSDoc added.

--- client/src/components/ProtectedAdminRoute.jsx
+++ client/src/components/ProtectedAdminRoute.jsx
@@ -3,6 +3,9 @@ import {useUserContext} from '../hooks/contextHooks';
 import { Children } from 'react';
 
 /**
+ * A component that protects admin routes.
+ * Only renders its children if the user is authenticated and has an admin role.
+ * Otherwise, redirects to home page for unauthenticated.
  * 
  * @param children - The components to render if the user is an admin.
  * @returns The children if user is admin, a loading message and a redirect to admin pages.
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,6 +2,8 @@ import {Navigate} from 'react-router';
 import {useUserContext} from '../hooks/contextHooks';
 
 /**
+ * A component that protects the routes for authenticated users.
+ * Otherwise, redirects to home page if the user is not logged in.
  * 
  * @param children - The components to render if the user is authenticated.
  * @returns The children if user is authenticated, or redirects to main page.
