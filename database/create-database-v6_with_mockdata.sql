188ff9e977e410c0378a1d9bd7d5e774cb64b08b
Quuikz <rkuikka@gmail.com>
2025-12-12 02:26:46 +0200

    Fixed mockdata

--- database/create-database-v6_with_mockdata.sql
+++ database/create-database-v6_with_mockdata.sql
@@ -255,30 +255,30 @@ INSERT INTO `locations` (`id`,`name`, `address`, `email`, `phone`, `table_count`
 -- -----------------------
 -- Table `meals`
 -- -----------------------
-INSERT INTO `meals` (`name_fi`, `name_en`, `description_fi`, `description_en`, `cost`, `image`, `message`) VALUES
-('Lihapulla', 'Meatballs', 'Perinteiset lihapullat', 'Classic meatballs', 12.5, 'meatballs.jpg','mock data from database'),
-('Lohta', 'Salmon', 'Paistettua lohta perunoilla', 'Fried salmon with potatoes', 15.0, 'salmon.jpg','mock data from database'),
-('Kasviskeitto', 'Vegetable Soup', 'Terveellinen kasviskeitto', 'Healthy vegetable soup', 8.0, 'veg_soup.jpg','mock data from database'),
-('Kana Curry', 'Chicken Curry', 'Mausteinen kanacurry riisillä', 'Spicy chicken curry with rice', 13.5, 'chicken_curry.jpg','mock data from database'),
-('Pasta Bolognese', 'Pasta Bolognese', 'Italialainen bolognese kastike pastan kanssa', 'Italian bolognese sauce with pasta', 11.0, 'pasta_bolognese.jpg','mock data from database'),
-('Pizza Margherita', 'Pizza Margherita', 'Perinteinen Margherita pizza', 'Classic Margherita pizza', 10.0, 'pizza_margherita.jpg','mock data from database'),
-('Pizza Pepperoni', 'Pizza Pepperoni', 'Pepperonilla täytetty pizza', 'Pepperoni pizza', 12.0, 'pizza_pepperoni.jpg','mock data from database'),
-('Caesar Salaatti', 'Caesar Salad', 'Salaatti kanalla ja parmesaanilla', 'Salad with chicken and parmesan', 9.5, 'caesar_salad.jpg','mock data from database'),
-('Lasagne', 'Lasagna', 'Italialainen lasagne', 'Italian lasagna', 14.0, 'lasagna.jpg','mock data from database'),
-('Kanasalaatti', 'Chicken Salad', 'Kevyt kanasalaatti', 'Light chicken salad', 10.5, 'chicken_salad.jpg','mock data from database'),
-('Tomaattikeitto', 'Tomato Soup', 'Terveellinen tomaattikeitto', 'Healthy tomato soup', 7.5, 'tomato_soup.jpg','mock data from database'),
-('Hampurilainen', 'Burger', 'Juustohampurilainen', 'Cheeseburger', 11.5, 'burger.jpg','mock data from database'),
-('Fish & Chips', 'Fish & Chips', 'Paistettua kalaa ja ranskalaisia', 'Fried fish and chips', 13.0, 'fish_chips.jpg','mock data from database'),
-('Sushi Set', 'Sushi Set', 'Assortment of sushi', 'Assortment of sushi', 18.0, 'sushi_set.jpg','mock data from database'),
-('Pannukakku', 'Pancake', 'Makea pannukakku', 'Sweet pancake', 6.0, 'pancake.jpg','mock data from database'),
-('Ratatouille', 'Ratatouille', 'Kasvisruoka ranskalaisittain', 'French-style vegetable dish', 12.0, 'ratatouille.jpg','mock data from database'),
-('Quiche', 'Quiche', 'Perinteinen quiche', 'Traditional quiche', 9.0, 'quiche.jpg','mock data from database'),
-('Risotto', 'Risotto', 'Sienirisotto', 'Mushroom risotto', 14.0, 'risotto.jpg','mock data from database'),
-('Tortilla', 'Tortilla', 'Täytetty tortilla', 'Stuffed tortilla', 10.0, 'tortilla.jpg','mock data from database'),
-('Porkkana-inkiväärikeitto', 'Carrot Ginger Soup', 'Mausteinen porkkana-inkiväärikeitto', 'Spicy carrot ginger soup', 8.0, 'carrot_ginger_soup.jpg','mock data from database'),
-('Grillattu Lohi', 'Grilled Salmon', 'Grillattua lohta ja vihanneksia', 'Grilled salmon with vegetables', 16.0, 'grilled_salmon.jpg','mock data from database'),
-('Beef Steak', 'Beef Steak', 'Paistettua pihviä', 'Grilled beef steak', 20.0, 'beef_steak.jpg','mock data from database'),
-('Veggie Burger', 'Veggie Burger', 'Kasvisburger', 'Vegetarian burger', 11.0, 'veggie_burger.jpg','mock data from database');
+INSERT INTO `meals` (`name_fi`, `name_en`, `description_fi`, `description_en`, `cost`, `type`, `image`, `message`) VALUES
+('Lihapulla', 'Meatballs', 'Perinteiset lihapullat', 'Classic meatballs', 12.5, 'G, M, E', 'meatballs.jpg','mock data from database'),
+('Lohta', 'Salmon', 'Paistettua lohta perunoilla', 'Fried salmon with potatoes', 15.0, 'F, L', 'salmon.jpg','mock data from database'),
+('Kasviskeitto', 'Vegetable Soup', 'Terveellinen kasviskeitto', 'Healthy vegetable soup', 8.0, 'CE', 'veg_soup.jpg','mock data from database'),
+('Kana Curry', 'Chicken Curry', 'Mausteinen kanacurry riisillä', 'Spicy chicken curry with rice', 13.5, 'M, S', 'chicken_curry.jpg','mock data from database'),
+('Pasta Bolognese', 'Pasta Bolognese', 'Italialainen bolognese kastike pastan kanssa', 'Italian bolognese sauce with pasta', 11.0, 'G, M, CE', 'pasta_bolognese.jpg','mock data from database'),
+('Pizza Margherita', 'Pizza Margherita', 'Perinteinen Margherita pizza', 'Classic Margherita pizza', 10.0, 'G, M', 'pizza_margherita.jpg','mock data from database'),
+('Pizza Pepperoni', 'Pizza Pepperoni', 'Pepperonilla täytetty pizza', 'Pepperoni pizza', 12.0, 'G, M, S', 'pizza_pepperoni.jpg','mock data from database'),
+('Caesar Salaatti', 'Caesar Salad', 'Salaatti kanalla ja parmesaanilla', 'Salad with chicken and parmesan', 9.5, 'F, M, E, G, MU', 'caesar_salad.jpg','mock data from database'),
+('Lasagne', 'Lasagna', 'Italialainen lasagne', 'Italian lasagna', 14.0, 'G, M, CE', 'lasagna.jpg','mock data from database'),
+('Kanasalaatti', 'Chicken Salad', 'Kevyt kanasalaatti', 'Light chicken salad', 10.5, 'E, MU', 'chicken_salad.jpg','mock data from database'),
+('Tomaattikeitto', 'Tomato Soup', 'Terveellinen tomaattikeitto', 'Healthy tomato soup', 7.5, NULL, 'tomato_soup.jpg','mock data from database'),
+('Hampurilainen', 'Burger', 'Juustohampurilainen', 'Cheeseburger', 11.5, 'G, M, E, S, SU', 'burger.jpg','mock data from database'),
+('Fish & Chips', 'Fish & Chips', 'Paistettua kalaa ja ranskalaisia', 'Fried fish and chips', 13.0, 'F, G, E', 'fish_chips.jpg','mock data from database'),
+('Sushi Set', 'Sushi Set', 'Assortment of sushi', 'Assortment of sushi', 18.0, 'F, S, SE, C', 'sushi_set.jpg','mock data from database'),
+('Pannukakku', 'Pancake', 'Makea pannukakku', 'Sweet pancake', 6.0, 'G, M, E', 'pancake.jpg','mock data from database'),
+('Ratatouille', 'Ratatouille', 'Kasvisruoka ranskalaisittain', 'French-style vegetable dish', 12.0, NULL, 'ratatouille.jpg','mock data from database'),
+('Quiche', 'Quiche', 'Perinteinen quiche', 'Traditional quiche', 9.0, 'G, M, E', 'quiche.jpg','mock data from database'),
+('Risotto', 'Risotto', 'Sienirisotto', 'Mushroom risotto', 14.0, NULL, 'risotto.jpg','mock data from database'),
+('Tortilla', 'Tortilla', 'Täytetty tortilla', 'Stuffed tortilla', 10.0, 'G', 'tortilla.jpg','mock data from database'),
+('Porkkana-inkiväärikeitto', 'Carrot Ginger Soup', 'Mausteinen porkkana-inkiväärikeitto', 'Spicy carrot ginger soup', 8.0, NULL, 'carrot_ginger_soup.jpg','mock data from database'),
+('Grillattu Lohi', 'Grilled Salmon', 'Grillattua lohta ja vihanneksia', 'Grilled salmon with vegetables', 16.0, 'F, M', 'grilled_salmon.jpg','mock data from database'),
+('Beef Steak', 'Beef Steak', 'Paistettua pihviä', 'Grilled beef steak', 20.0, 'M, SU', 'beef_steak.jpg','mock data from database'),
+('Veggie Burger', 'Veggie Burger', 'Kasvisburger', 'Vegetarian burger', 11.0, 'G, M, S, E, SE', 'veggie_burger.jpg','mock data from database');
 
 -- -----------------------
 -- Table `orders`
